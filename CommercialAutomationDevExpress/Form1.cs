6da0771b1b251542ba75b18ce8c9405951070fbe
Erdem Altıparmak <erdem52ap@gmail.com>
2021-01-31 10:01:39 +0300

    .

--- CommercialAutomationDevExpress/Form1.cs
+++ CommercialAutomationDevExpress/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace CommercialAutomationDevExpress
 {
-    public partial class Form1 : Form
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
     {
         public Form1()
         {
